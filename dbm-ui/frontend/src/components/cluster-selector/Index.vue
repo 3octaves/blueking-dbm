9756a5a08d13c7eeb372958c608f418fa2131406
3octaves <873551943@qq.com>
2024-09-26 12:02:05 +0800

    fix(frontend): mongo验收问题修复 #6052

--- dbm-ui/frontend/src/components/cluster-selector/Index.vue
+++ dbm-ui/frontend/src/components/cluster-selector/Index.vue
@@ -305,7 +305,7 @@
     },
     [ClusterTypes.MONGO_REPLICA_SET]: {
       id: ClusterTypes.MONGO_REPLICA_SET,
-      name: t('集群选择'),
+      name: t('副本集'),
       disabledRowConfig: [
         {
           handler: (data: T) => data.isOffline,
@@ -319,7 +319,7 @@
     },
     [ClusterTypes.MONGO_SHARED_CLUSTER]: {
       id: ClusterTypes.MONGO_SHARED_CLUSTER,
-      name: t('集群选择'),
+      name: t('分片集群'),
       disabledRowConfig: [
         {
           handler: (data: T) => data.isOffline,
